dc668942d3a02e8b99ccba10e29b21a4f2c03c04
CharudattaGhute <ghutecharudatta@gmail.com>
2024-08-03 16:03:51 +0530

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -41,7 +41,7 @@ function Register({ onRegister }) {
         width: "40%",
         height: "460px",
         marginTop: "10%",
-        backgroundColor: "aqua",
+        backgroundColor: "#EAE7DC",
         borderRadius: "20px",
       }}
     >
